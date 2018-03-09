04216640e173dd7ae5e2675afe5c588c09e11b8f
Thomas Nathan CT <32567705+TNathanCT@users.noreply.github.com>
2018-03-09 15:53:21 +0000

    Add files via upload

---
+++ Basic First Scripts/Enemy Scripts/EnemyChase.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MonsterPatrol))]
+public class EnemyChase : MonoBehaviour
+{
+
+    public Transform player;
+    public float playerDistance;
+    public float rotationDampling;
+    public float moveSpeed;
+    public float lookDistance;
+    public float chaseDistance;
+    public float stopChaseDistance;
+    MonsterPatrol MP;
+    public Transform SpawnPoint;
+
+    bool AtStartingPosition;
+
+    public GameObject Enemy;
+
+
+    private void Start()
+    {
+        MP = GetComponent<MonsterPatrol>();
+        Enemy.transform.position = SpawnPoint.transform.position;
+    }
+
+    void Update()
+    {
+        if (Enemy.transform.position == SpawnPoint.transform.position)
+        {
+            MP.enabled = false;
+        }
+
+
+        playerDistance = Vector3.Distance(player.position, transform.position);
+
+        if (playerDistance < lookDistance)
+        {
+            lookAtPlayer();
+        }
+
+        if (playerDistance < chaseDistance)
+        {
+
+            chase();
+        }
+
+        if (playerDistance > stopChaseDistance && Enemy.transform.position != SpawnPoint.transform.position)
+        {
+            MP.enabled = true;
+        }
+
+    }
+
+    void lookAtPlayer()
+    {
+
+        Quaternion rotation = Quaternion.LookRotation(player.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationDampling);
+    }
+
+    void chase()
+    {
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+    }
+}
+
