1bf68dc3e0af3a3f07cbb62d90fc49a1e6ab6b40
Thomas Nathan CT <32567705+TNathanCT@users.noreply.github.com>
2018-03-09 15:53:48 +0000

    Add files via upload

---
+++ Basic First Scripts/Enemy Scripts/MonsterPatrol.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterPatrol : MonoBehaviour {
+    public float moveSpeed;
+    public Transform[] PatrolPoints;
+    private int CurrentPoint;
+
+    void Start()
+    {
+        transform.position = PatrolPoints[0].position;
+        CurrentPoint = 0;
+    }
+
+
+    void Update()
+    {
+
+        if (CurrentPoint >= PatrolPoints.Length)
+        {
+            CurrentPoint = 0;
+        }
+
+        if (transform.position == PatrolPoints[CurrentPoint].position)
+        {
+            CurrentPoint++;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, PatrolPoints[CurrentPoint].position, moveSpeed * Time.deltaTime);
+
+    }
+}
