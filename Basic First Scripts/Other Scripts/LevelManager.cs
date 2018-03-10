da9bf25dc7ae1adb944e72e79e3b57b33bd57cfd
Thomas Nathan CT <32567705+TNathanCT@users.noreply.github.com>
2018-03-10 14:47:50 +0000

    Add files via upload

---
+++ Basic First Scripts/Other Scripts/LevelManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public Rigidbody Drone;
+    public Transform SpawnDrone;
+    public bool IsthereaDrone;
+
+    public GameObject PlayerCamera;
+    public PlayerController PC;
+    CameraRotation ML;
+    Hologram HG;
+
+    void Start()
+    {
+        PC = FindObjectOfType<PlayerController>();
+        ML = FindObjectOfType<CameraRotation>();
+        IsthereaDrone = false;
+        HG = FindObjectOfType<Hologram>();
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(2) && IsthereaDrone == false)
+        {
+            PlayerCamera.SetActive(false);
+            IsthereaDrone = true;
+            Drone.transform.position = SpawnDrone.transform.position;
+            ML.enabled = false;
+            HG.enabled = false;
+        }
+
+        else if (IsthereaDrone == true && Input.GetMouseButtonUp(2)) {
+
+            PlayerCamera.SetActive(true);
+            ML.enabled = true;
+            IsthereaDrone = false;
+            HG.enabled = true;
+        }
+
+
+    }
+}
