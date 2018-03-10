c4de05bf32e8daf3145b2b2275b89aacfa6dbe40
Thomas Nathan CT <32567705+TNathanCT@users.noreply.github.com>
2018-03-10 14:45:15 +0000

    Add files via upload

---
+++ Basic First Scripts/Player Scripts/PlayerHP.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHP : MonoBehaviour {
+
+    public int count;
+
+    public GameObject Sparks;
+    public GameObject Smoke;
+    public GameObject Fire;
+
+
+	void Start () {
+        count = 0;
+        Sparks.SetActive(false);
+        Smoke.SetActive(false);
+        Fire.SetActive(false);
+	}
+	
+	void Update () {
+        if(count == 1)
+        {
+            Sparks.SetActive(true);
+        }
+
+        if(count == 2)
+        {
+            Smoke.SetActive(true);
+        }
+
+        if(count == 3)
+        {
+            Fire.SetActive(true);
+        }
+		
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            count += 1;
+        } 
+    }
+}
