3e741343555a9233c0742be6c0e6e1ca40ed14c1
Thomas Nathan CT <32567705+TNathanCT@users.noreply.github.com>
2018-03-10 14:46:24 +0000

    Add files via upload

---
+++ Basic First Scripts/Player Scripts/Hologram.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hologram : MonoBehaviour {
+
+    public GameObject Holo;
+
+	void Start () {
+        Holo.SetActive(false);
+		
+	}
+	
+	
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            Holo.SetActive(true);
+        }
+        else
+        {
+            Holo.SetActive(false);
+        }
+		
+	}
+}
